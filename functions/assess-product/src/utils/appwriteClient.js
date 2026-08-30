4663f85ebea0717dc762b9c7a1bb2b04c1578d9e
Prince Biile <biileprinceyennuyar5@gmail.com>
2026-08-30 00:37:42 +0000

    fix(cloud-function): add fallback to system env var APPWRITE_FUNCTION_PROJECT_ID

--- functions/assess-product/src/utils/appwriteClient.js
+++ functions/assess-product/src/utils/appwriteClient.js
@@ -3,7 +3,7 @@ import { Client, Databases, Storage } from 'node-appwrite';
 export function getAppwriteClient() {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
-    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setProject(process.env.APPWRITE_PROJECT_ID || process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
 
   const databases = new Databases(client);
