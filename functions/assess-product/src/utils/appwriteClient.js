bdb734934cb1ce58ebdce2f1af74845d942f834f
Prince Biile <biileprinceyennuyar5@gmail.com>
2026-08-30 01:27:32 +0000

    fix(cloud-function): prioritize APPWRITE_FUNCTION_ENDPOINT to fix fetch failed error

--- functions/assess-product/src/utils/appwriteClient.js
+++ functions/assess-product/src/utils/appwriteClient.js
@@ -2,7 +2,7 @@ import { Client, Databases, Storage } from 'node-appwrite';
 
 export function getAppwriteClient() {
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
+    .setEndpoint(process.env.APPWRITE_FUNCTION_ENDPOINT || process.env.APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
     .setProject(process.env.APPWRITE_PROJECT_ID || process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
 
